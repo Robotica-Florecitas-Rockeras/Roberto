56ccf16fc40bf058298d2b5265c20320da8d888f
Brayan Joya <b.joya@uniandes.edu.co>
2025-03-26 04:37:34 -0500

    Nodos creados

---
+++ src/roberto/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'roberto'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='elipson',
+    maintainer_email='elipson@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "roberto_teleop = roberto.roberto_teleop:main",
+            "roberto_interface = roberto.roberto_interface:main",
+            "roberto_player = roberto.roberto_player:main"
+        ],
+    },
+)
